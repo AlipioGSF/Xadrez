d8f95ede33f84a7028b328306d9899958249f333
Alipio Gomes <alipiotec@gmail.com>
2023-01-04 16:23:06 -0300

    criada class ChessPosition e placeNewPiece() em ChessMarch

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,12 +24,14 @@ public class ChessMatch {
 	
 	}
 	
-	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 2));
-		board.placePiece(new King(board, Color.WHITE), new Position(0, 2));
-		
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	
+	private void initialSetup() {
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.BLACK));
 	}
 	
 }
